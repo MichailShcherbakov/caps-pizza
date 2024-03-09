6f22f686c1112c390573f451eed1cffb31cdf832
Sherbakov Mikhail <michailshcherbakov@gmail.com>
2024-03-09 19:11:28 +0300

    fix(logs): improve server side logs

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -16,7 +16,10 @@ async function bootstrap() {
     await NestFactory.create(AppModule, {
       logger: WinstonModule.createLogger({
         level: "info",
-        format: winston.format.json(),
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.json()
+        ),
         transports: [
           new winston.transports.Console({
             format: winston.format.combine(
