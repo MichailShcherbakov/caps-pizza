fe6fe4ba731c616febee1a42b6570e0627b68e51
Sherbakov Mikhail <michailshcherbakov@gmail.com>
2024-04-02 19:18:12 +0300

    feat(gtm): link google tag manager

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -1,12 +1,27 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionCache from "~/helpers/create-emotion-cache";
 import createEmotionServer from "@emotion/server/create-instance";
 
 export default class RootDocument extends Document {
+  readonly GTM_TOKEN = "GTM-PXN6X6F";
+
   render() {
     return (
       <Html lang="ru">
         <Head>
+          <script
+            id="gtm"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${this.GTM_TOKEN}');
+              `,
+            }}
+          />
           <link rel="shortcut icon" href="/favicon.ico" />
           <link
             rel="stylesheet"
@@ -26,6 +41,14 @@ export default class RootDocument extends Document {
           {(this.props as any).emotionStyleTags}
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${this.GTM_TOKEN}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
