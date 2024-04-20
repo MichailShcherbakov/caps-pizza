dd1a5b4057e5c3e84338bb87b33a92560e7bc655
Sherbakov Mikhail <michailshcherbakov@gmail.com>
2024-04-20 15:27:12 +0300

    feat(metrics): add yandex metrics counter

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable @next/next/next-script-for-ga */
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionCache from "~/helpers/create-emotion-cache";
@@ -38,6 +39,35 @@ export default class RootDocument extends Document {
             rel="stylesheet"
           />
           <meta name="emotion-insertion-point" content="" />
+          <script
+            id="yandex.metrika counter"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+              
+                ym(96939630, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true,
+                  ecommerce:"dataLayer"
+                });
+              `,
+            }}
+          />
+          <noscript>
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/96939630"
+                style={{ position: "absolute", left: "-9999px" }}
+                alt=""
+              />
+            </div>
+          </noscript>
           {(this.props as any).emotionStyleTags}
         </Head>
         <body>
